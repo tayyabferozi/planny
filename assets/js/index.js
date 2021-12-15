d399096bfa50a02b12174b3b4ed3a6d55d15531c
Tayyab Ferozi <ferozitayyab@gmail.com>
2021-12-16 01:29:23 +0500

    complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,40 @@
+function openOverlay() {
+  $("#dark-overlay").removeClass("d-none");
+}
+
+function closeOverlay() {
+  $("#dark-overlay").addClass("d-none");
+}
+
 $(".custom-modal .close").click(function () {
   $("#dark-overlay").addClass("d-none");
 });
 
 $("#right-menu-login .close,#right-menu .close").click(function () {
-  $("#dark-overlay").addClass("d-none");
+  $("#dark-overlay-beneath").addClass("d-none");
   $(this).parent().addClass("d-none");
 });
+
+$(".slider_text_cont_p1 [src='./assets/images/message.svg']").click(
+  function () {
+    openOverlay();
+    $("#message-modal").removeClass("d-none");
+  }
+);
+
+$(".slider_text_cont_p1 [src='./assets/images/call.svg']").click(function () {
+  openOverlay();
+  $("#call-modal").removeClass("d-none");
+});
+
+$("[src='./assets/images/user.svg']").click(function () {
+  openOverlay();
+  $("#signup-modal").removeClass("d-none");
+});
+
+$("[data-target='#exampleModal6']").click(function () {
+  $("#dark-overlay-beneath").removeClass("d-none");
+  $("#right-menu-login").removeClass("d-none");
+});
+
+$("input[type='time']").timepicki({ reset: true });
